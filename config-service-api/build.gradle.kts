24da91654175e92fed9d3ed0afb37c22ea48142c
pavan-traceable <73101820+pavan-traceable@users.noreply.github.com>
2021-02-23 20:58:18 +0530

    Publish proto convereter and config-service (#26)
    * Pubish proto convereter and config-service

--- config-service-api/build.gradle.kts
+++ config-service-api/build.gradle.kts
@@ -2,6 +2,7 @@ import com.google.protobuf.gradle.*
 
 plugins {
   `java-library`
+  `java-test-fixtures`
   id("com.google.protobuf") version "0.8.13"
   id("org.hypertrace.publish-plugin")
 }
@@ -58,10 +59,19 @@ sourceSets {
 dependencies {
   api("io.grpc:grpc-protobuf:1.35.0")
   api("io.grpc:grpc-stub:1.35.0")
+  api("javax.annotation:javax.annotation-api:1.3.2")
   constraints {
     implementation("com.google.guava:guava:30.1-jre") {
       because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415")
     }
   }
-  api("javax.annotation:javax.annotation-api:1.3.2")
+  testFixturesApi("io.grpc:grpc-api:1.35.0")
+  testFixturesApi(project(":config-service-api"))
+  testFixturesImplementation("io.grpc:grpc-stub:1.35.0")
+  testFixturesImplementation("io.grpc:grpc-core:1.35.0")
+  testFixturesImplementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.3.3")
+  testFixturesImplementation("org.mockito:mockito-core:3.7.0")
+  testFixturesImplementation("com.google.guava:guava:30.1-jre")
+  testFixturesAnnotationProcessor("org.projectlombok:lombok:1.18.12")
+  testFixturesCompileOnly("org.projectlombok:lombok:1.18.12")
 }
