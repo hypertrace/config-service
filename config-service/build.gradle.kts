d291061b033ff89278a0a0229165ada1496e43c7
Rishabh Singh <rishabh@traceable.ai>
2021-08-04 11:50:10 +0530

    Add platform-metrics dep (#45)

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
   implementation(project(":spaces-config-service-impl"))
   implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.4.0")
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.23")
   implementation("com.typesafe:config:1.4.1")
   implementation("org.slf4j:slf4j-api:1.7.30")
 
