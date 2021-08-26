37373f4148571f1ec37f2e3e870917e027b59743
kamaleshnneerasa <35007783+kamaleshnneerasa@users.noreply.github.com>
2021-08-26 14:49:19 +0530

    Added default system tags (#49)
    * Added default system tags
    
    * Added functionality to read an array of system tags from application.conf file
    
    * Addressing PR comments
    
    * Addressing PR comments-1
    
    * Chanined exception assert statement in tests
    
    Co-authored-by: kamaleshnneerasa <kamaleshnneerasa@github.com>

--- tags-config-service-impl/src/test/java/org/hypertrace/tag/config/service/TagConfigServiceImplTest.java
+++ tags-config-service-impl/src/test/java/org/hypertrace/tag/config/service/TagConfigServiceImplTest.java
@@ -1,10 +1,17 @@
 package org.hypertrace.tag.config.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import io.grpc.Channel;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.hypertrace.config.service.test.MockGenericConfigService;
@@ -26,7 +33,12 @@ import org.junit.jupiter.api.Test;
 
 public final class TagConfigServiceImplTest {
   TagConfigServiceBlockingStub tagConfigStub;
+  Config config;
   MockGenericConfigService mockGenericConfigService;
+  List<Tag> systemTags =
+      Arrays.asList("Critical", "Sensitive", "External", "Sentry").stream()
+          .map(key -> Tag.newBuilder().setId(key).setKey(key).build())
+          .collect(Collectors.toList());
   List<CreateTag> createTagsList =
       Arrays.asList(0, 1, 2, 3, 4).stream()
           .map(id -> CreateTag.newBuilder().setKey("Tag-" + id).build())
@@ -36,8 +48,17 @@ public final class TagConfigServiceImplTest {
   void setUp() {
     mockGenericConfigService =
         new MockGenericConfigService().mockUpsert().mockGet().mockGetAll().mockDelete();
+    Map<String, List<Map<String, String>>> systemTagsConfigMap = new HashMap<>();
+    systemTagsConfigMap.put(
+        "system.tags",
+        systemTags.stream()
+            .map(systemTag -> Map.of("id", systemTag.getId(), "key", systemTag.getKey()))
+            .collect(Collectors.toList()));
+    Map<String, Object> configMap = new HashMap<>();
+    configMap.put("tag.config.service", systemTagsConfigMap);
+    config = ConfigFactory.parseMap(configMap);
     Channel channel = mockGenericConfigService.channel();
-    mockGenericConfigService.addService(new TagConfigServiceImpl(channel)).start();
+    mockGenericConfigService.addService(new TagConfigServiceImpl(channel, config)).start();
     tagConfigStub = TagConfigServiceGrpc.newBlockingStub(channel);
   }
 
@@ -68,6 +89,23 @@ public final class TagConfigServiceImplTest {
   }
 
   @Test
+  void test_system_createTag() {
+    for (Tag systemTag : systemTags) {
+      CreateTagRequest createTagRequest =
+          CreateTagRequest.newBuilder()
+              .setTag(CreateTag.newBuilder().setKey(systemTag.getKey()).build())
+              .build();
+      Throwable exception =
+          assertThrows(
+              StatusRuntimeException.class,
+              () -> {
+                tagConfigStub.createTag(createTagRequest);
+              });
+      assertEquals(Status.ALREADY_EXISTS, Status.fromThrowable(exception));
+    }
+  }
+
+  @Test
   void test_getTag() {
     List<Tag> createdTagsList = createTags();
     // Querying each tag one by one for the created or inserted tags in the previous step
@@ -83,19 +121,28 @@ public final class TagConfigServiceImplTest {
             .collect(Collectors.toList());
     assertEquals(createdTagsList, getTagList);
     // Get a tag that does not exist
-    String exceptionMessage = "NONE";
-    try {
-      tagConfigStub.getTag(GetTagRequest.newBuilder().setId("1").build());
-    } catch (Exception e) {
-      exceptionMessage = e.getMessage();
-      System.out.println(e);
+    Throwable exception =
+        assertThrows(
+            StatusRuntimeException.class,
+            () -> {
+              tagConfigStub.getTag(GetTagRequest.newBuilder().setId("1").build());
+            });
+    assertEquals(Status.NOT_FOUND, Status.fromThrowable(exception));
+  }
+
+  @Test
+  void test_system_getTag() {
+    for (Tag systemTag : systemTags) {
+      GetTagRequest getTagRequest = GetTagRequest.newBuilder().setId(systemTag.getId()).build();
+      GetTagResponse getTagResponse = tagConfigStub.getTag(getTagRequest);
+      assertEquals(systemTag, getTagResponse.getTag());
     }
-    assertEquals("NOT_FOUND", exceptionMessage);
   }
 
   @Test
   void test_getTags() {
     List<Tag> createdTagsList = createTags();
+    createdTagsList.addAll(systemTags);
     // Querying for all the tags at once for the created or inserted tags in the previous step
     List<Tag> getTags = tagConfigStub.getTags(GetTagsRequest.newBuilder().build()).getTagsList();
     assertEquals(Set.copyOf(createdTagsList), Set.copyOf(getTags));
@@ -121,31 +168,64 @@ public final class TagConfigServiceImplTest {
             .collect(Collectors.toList());
     assertEquals(updateTagsList, updatedTagsList);
     // Updating a tag that does not exist
-    String exceptionMessage = "NONE";
-    try {
-      tagConfigStub.updateTag(
+    Throwable exception =
+        assertThrows(
+            StatusRuntimeException.class,
+            () -> {
+              tagConfigStub.updateTag(
+                  UpdateTagRequest.newBuilder()
+                      .setTag(Tag.newBuilder().setId("1").setKey("API-X").build())
+                      .build());
+            });
+    assertEquals(Status.NOT_FOUND, Status.fromThrowable(exception));
+  }
+
+  @Test
+  void test_system_updateTag() {
+    List<Tag> createdTagsList = createTags();
+    for (Tag systemTag : systemTags) {
+      UpdateTagRequest updateTagRequest =
           UpdateTagRequest.newBuilder()
-              .setTag(Tag.newBuilder().setId("1").setKey("API-X").build())
-              .build());
-    } catch (Exception e) {
-      exceptionMessage = e.getMessage();
-      System.out.println(e);
+              .setTag(Tag.newBuilder().setId(systemTag.getId()).setKey("1").build())
+              .build();
+      Throwable exception =
+          assertThrows(
+              StatusRuntimeException.class,
+              () -> {
+                tagConfigStub.updateTag(updateTagRequest);
+              });
+      assertEquals(Status.INVALID_ARGUMENT, Status.fromThrowable(exception));
     }
-    assertEquals("NOT_FOUND", exceptionMessage);
+    for (Tag systemTag : systemTags) {
+      UpdateTagRequest updateTagRequest =
+          UpdateTagRequest.newBuilder()
+              .setTag(
+                  Tag.newBuilder()
+                      .setId(createdTagsList.get(0).getId())
+                      .setKey(systemTag.getKey())
+                      .build())
+              .build();
+      Throwable exception =
+          assertThrows(
+              StatusRuntimeException.class,
+              () -> {
+                tagConfigStub.updateTag(updateTagRequest);
+              });
+      assertEquals(Status.INVALID_ARGUMENT, Status.fromThrowable(exception));
+    }
   }
 
   @Test
   void test_deleteTag() {
     List<Tag> createdTagsList = createTags();
     // Deleting a tag that does not exist
-    String exceptionMessage = "NONE";
-    try {
-      tagConfigStub.deleteTag(DeleteTagRequest.newBuilder().setId("1").build());
-    } catch (Exception e) {
-      exceptionMessage = e.getMessage();
-      System.out.println(e);
-    }
-    assertEquals("NOT_FOUND", exceptionMessage);
+    Throwable exception =
+        assertThrows(
+            StatusRuntimeException.class,
+            () -> {
+              tagConfigStub.deleteTag(DeleteTagRequest.newBuilder().setId("1").build());
+            });
+    assertEquals(Status.NOT_FOUND, Status.fromThrowable(exception));
     // Deleting each tag one by one and verifying the delete operation
     createdTagsList.stream()
         .forEach(
@@ -156,6 +236,21 @@ public final class TagConfigServiceImplTest {
               assertEquals(false, allTags.contains(tag));
             });
     List<Tag> allTags = tagConfigStub.getTags(GetTagsRequest.newBuilder().build()).getTagsList();
-    assertEquals(true, allTags.isEmpty());
+    assertEquals(systemTags, allTags);
+  }
+
+  @Test
+  void test_system_deleteTag() {
+    for (Tag systemTag : systemTags) {
+      DeleteTagRequest deleteTagRequest =
+          DeleteTagRequest.newBuilder().setId(systemTag.getId()).build();
+      Throwable exception =
+          assertThrows(
+              StatusRuntimeException.class,
+              () -> {
+                tagConfigStub.deleteTag(deleteTagRequest);
+              });
+      assertEquals(Status.INVALID_ARGUMENT, Status.fromThrowable(exception));
+    }
   }
 }
