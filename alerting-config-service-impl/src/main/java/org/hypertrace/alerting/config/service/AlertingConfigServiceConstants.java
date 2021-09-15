99b7bfa4c60ce3bc34a58379f4e71e4aabc38024
rish691 <rishabh@traceable.ai>
2021-09-15 18:30:23 +0530

    rename vars in constant

--- alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/AlertingConfigServiceConstants.java
+++
@@ -1,8 +0,0 @@
-package org.hypertrace.alerting.config.service;
-
-public interface AlertingConfigServiceConstants {
-  String ALERTING_CONFIG_SERVICE_CONFIG = "alerting.configuration";
-  String ALERTING_RULE_CONFIG_RESOURCE_NAME = "alertingRuleConfig";
-  String ALERTING_CHANNEL_CONFIG_RESOURCE_NAME = "alertingChannelConfig";
-  String ALERTING_CONFIG_NAMESPACE = "alerting-v1";
-}
---
+++ alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/NotificationConfigServiceConstants.java
@@ -0,0 +1,7 @@
+package org.hypertrace.alerting.config.service;
+
+public interface NotificationConfigServiceConstants {
+  String NOTIFICATION_RULE_CONFIG_RESOURCE_NAME = "notificationRuleConfig";
+  String NOTIFICATION_CHANNEL_CONFIG_RESOURCE_NAME = "notificationChannelConfig";
+  String NOTIFICATION_CONFIG_NAMESPACE = "notification-v1";
+}
