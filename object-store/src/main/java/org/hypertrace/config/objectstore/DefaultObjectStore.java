d0030c1ccc86c26dac76439b7abe1f46f38c2d5e
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-09-20 06:46:03 -0700

    refactor: add object store abstractions (#58)

---
+++ object-store/src/main/java/org/hypertrace/config/objectstore/DefaultObjectStore.java
@@ -0,0 +1,84 @@
+package org.hypertrace.config.objectstore;
+
+import com.google.protobuf.Value;
+import io.grpc.Status;
+import java.util.Optional;
+import org.hypertrace.config.service.v1.ConfigServiceGrpc.ConfigServiceBlockingStub;
+import org.hypertrace.config.service.v1.DeleteConfigRequest;
+import org.hypertrace.config.service.v1.GetConfigRequest;
+import org.hypertrace.config.service.v1.UpsertConfigRequest;
+import org.hypertrace.core.grpcutils.context.RequestContext;
+
+/**
+ * DefaultObjectStore is an abstraction over the grpc api for config implementations working with
+ * single object with no identifier - a pattern often used for a tenant-wide config.
+ *
+ * @param <T>
+ */
+public abstract class DefaultObjectStore<T> {
+  private final ConfigServiceBlockingStub configServiceBlockingStub;
+  private final String resourceNamespace;
+  private final String resourceName;
+
+  protected DefaultObjectStore(
+      ConfigServiceBlockingStub configServiceBlockingStub,
+      String resourceNamespace,
+      String resourceName) {
+    this.configServiceBlockingStub = configServiceBlockingStub;
+    this.resourceNamespace = resourceNamespace;
+    this.resourceName = resourceName;
+  }
+
+  protected abstract Optional<T> buildObjectFromValue(Value value);
+
+  protected abstract Value buildValueFromObject(T object);
+
+  public Optional<T> getObject(RequestContext context) {
+    try {
+      Value value =
+          context.call(
+              () ->
+                  this.configServiceBlockingStub
+                      .getConfig(
+                          GetConfigRequest.newBuilder()
+                              .setResourceName(this.resourceName)
+                              .setResourceNamespace(this.resourceNamespace)
+                              .build())
+                      .getConfig());
+      T object = this.buildObjectFromValue(value).orElseThrow(Status.INTERNAL::asRuntimeException);
+      return Optional.of(object);
+    } catch (Exception exception) {
+      if (Status.fromThrowable(exception).equals(Status.NOT_FOUND)) {
+        return Optional.empty();
+      }
+      throw exception;
+    }
+  }
+
+  public T upsertObject(RequestContext context, T object) {
+    Value upsertedValue =
+        context.call(
+            () ->
+                this.configServiceBlockingStub
+                    .upsertConfig(
+                        UpsertConfigRequest.newBuilder()
+                            .setResourceName(this.resourceName)
+                            .setResourceNamespace(this.resourceNamespace)
+                            .setConfig(this.buildValueFromObject(object))
+                            .build())
+                    .getConfig());
+
+    return this.buildObjectFromValue(upsertedValue)
+        .orElseThrow(Status.INTERNAL::asRuntimeException);
+  }
+
+  public void deleteObject(RequestContext context) {
+    context.call(
+        () ->
+            this.configServiceBlockingStub.deleteConfig(
+                DeleteConfigRequest.newBuilder()
+                    .setResourceName(this.resourceName)
+                    .setResourceNamespace(this.resourceNamespace)
+                    .build()));
+  }
+}
