47f8d71bd25cf4f9dc05b90dd405d4cf56ef58f0
rish691 <rishabh@traceable.ai>
2021-09-21 18:00:14 +0530

    Use validator util

--- alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/AlertingConfigServiceConstants.java
+++
@@ -1,6 +0,0 @@
-package org.hypertrace.alerting.config.service;
-
-interface AlertingConfigServiceConstants {
-  String ALERTING_EVENT_CONDITION_CONFIG_RESOURCE_NAME = "alertingEventConditionConfig";
-  String ALERTING_CONFIG_NAMESPACE = "alerting-v1";
-}
--- alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/EventConditionConfigServiceRequestValidator.java
+++ alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/EventConditionConfigServiceRequestValidator.java
@@ -1,17 +1,20 @@
 package org.hypertrace.alerting.config.service;
 
+import static org.hypertrace.config.validation.GrpcValidatorUtils.validateRequestContextOrThrow;
+
 import com.google.common.base.Preconditions;
 import org.hypertrace.alerting.config.service.v1.CreateEventConditionRequest;
 import org.hypertrace.alerting.config.service.v1.DeleteEventConditionRequest;
 import org.hypertrace.alerting.config.service.v1.GetAllEventConditionsRequest;
 import org.hypertrace.alerting.config.service.v1.UpdateEventConditionRequest;
+import org.hypertrace.config.validation.GrpcValidatorUtils;
 import org.hypertrace.core.grpcutils.context.RequestContext;
 
 public class EventConditionConfigServiceRequestValidator {
 
   public void validateCreateEventConditionRequest(
       RequestContext requestContext, CreateEventConditionRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(request.hasNewEventCondition(), "EventCondition should be present");
     Preconditions.checkArgument(
         request.getNewEventCondition().getEventConditionData().getMetricAnomalyEventCondition()
@@ -21,7 +24,7 @@ public class EventConditionConfigServiceRequestValidator {
 
   public void validateUpdateEventConditionRequest(
       RequestContext requestContext, UpdateEventConditionRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(request.hasEventCondition(), "EventCondition should be present");
     Preconditions.checkArgument(
         request.getEventCondition().getEventConditionData().getMetricAnomalyEventCondition()
@@ -31,18 +34,13 @@ public class EventConditionConfigServiceRequestValidator {
 
   public void validateGetAllEventConditionsRequest(
       RequestContext requestContext, GetAllEventConditionsRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
   }
 
   public void validateDeleteEventConditionRequest(
       RequestContext requestContext, DeleteEventConditionRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         !request.getEventConditionId().isBlank(), "EventCondition Id cannot be empty");
-  }
-
-  private void validateTenantID(RequestContext requestContext) {
-    Preconditions.checkArgument(
-        requestContext.getTenantId().isPresent(), "Tenant ID is not present in request context");
   }
 }
