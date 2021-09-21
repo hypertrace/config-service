3dd9e9dc24430c848951bd0757a418fe405b42fa
rish691 <rishabh@traceable.ai>
2021-09-21 17:02:44 +0530

    Package private constants

--- alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/AlertingConfigServiceConstants.java
+++ alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/AlertingConfigServiceConstants.java
@@ -1,6 +1,6 @@
 package org.hypertrace.alerting.config.service;
 
-public interface AlertingConfigServiceConstants {
+interface AlertingConfigServiceConstants {
   String ALERTING_EVENT_CONDITION_CONFIG_RESOURCE_NAME = "alertingEventConditionConfig";
   String ALERTING_CONFIG_NAMESPACE = "alerting-v1";
 }
