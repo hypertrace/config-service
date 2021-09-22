1fae29867f020fb8908cc10fc5b289166f9a0249
rish691 <rishabh@traceable.ai>
2021-09-22 09:46:31 +0530

    review comments

--- alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/EventConditionStore.java
+++ alerting-config-service-impl/src/main/java/org/hypertrace/alerting/config/service/EventConditionStore.java
@@ -4,17 +4,13 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Value;
 import io.grpc.Channel;
 import java.util.Optional;
-import java.util.UUID;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.hypertrace.alerting.config.service.v1.EventCondition;
-import org.hypertrace.alerting.config.service.v1.EventConditionMutableData;
-import org.hypertrace.alerting.config.service.v1.EventConditionMutableData.ConditionCase;
-import org.hypertrace.alerting.config.service.v1.NewEventCondition;
 import org.hypertrace.config.objectstore.IdentifiedObjectStore;
 import org.hypertrace.config.proto.converter.ConfigProtoConverter;
 import org.hypertrace.config.service.v1.ConfigServiceGrpc;
 import org.hypertrace.core.grpcutils.client.RequestContextClientCallCredsProviderFactory;
-import org.hypertrace.core.grpcutils.context.RequestContext;
 
 @Slf4j
 public class EventConditionStore extends IdentifiedObjectStore<EventCondition> {
@@ -32,27 +28,6 @@ public class EventConditionStore extends IdentifiedObjectStore<EventCondition> {
         ALERTING_EVENT_CONDITION_CONFIG_RESOURCE_NAME);
   }
 
-  public EventCondition createEventCondition(
-      RequestContext requestContext, NewEventCondition newEventCondition) {
-    EventCondition.Builder builder = EventCondition.newBuilder();
-    if (newEventCondition.getEventConditionData().getConditionCase()
-        == ConditionCase.METRIC_ANOMALY_EVENT_CONDITION) {
-      builder.setEventConditionData(
-          EventConditionMutableData.newBuilder()
-              .setMetricAnomalyEventCondition(
-                  newEventCondition.getEventConditionData().getMetricAnomalyEventCondition()));
-    } else {
-      throw new RuntimeException(
-          String.format(
-              "Condition type is incorrect: %s",
-              newEventCondition.getEventConditionData().getConditionCase().name()));
-    }
-
-    builder.setId(UUID.randomUUID().toString());
-
-    return upsertObject(requestContext, builder.build());
-  }
-
   @Override
   protected Optional<EventCondition> buildObjectFromValue(Value value) {
     EventCondition.Builder builder = EventCondition.newBuilder();
@@ -60,19 +35,15 @@ public class EventConditionStore extends IdentifiedObjectStore<EventCondition> {
       ConfigProtoConverter.mergeFromValue(value, builder);
       return Optional.of(builder.build());
     } catch (InvalidProtocolBufferException e) {
-      log.error("Conversion failed. value {}, error {}", value, e);
+      log.error("Conversion failed. value {}", value, e);
       return Optional.empty();
     }
   }
 
+  @SneakyThrows
   @Override
   protected Value buildValueFromObject(EventCondition object) {
-    try {
-      return ConfigProtoConverter.convertToValue(object);
-    } catch (InvalidProtocolBufferException e) {
-      log.error("Conversion failed. value {}, error {}", object, e);
-    }
-    return null;
+    return ConfigProtoConverter.convertToValue(object);
   }
 
   @Override
