1fa3d60deef51ceb585bea97594669b8c9509004
rish691 <rishabh@traceable.ai>
2021-09-22 13:16:13 +0530

    use validation utils

--- notification-config-service-impl/src/main/java/org/hypertrace/notification/config/service/NotificationConfigServiceRequestValidator.java
+++ notification-config-service-impl/src/main/java/org/hypertrace/notification/config/service/NotificationConfigServiceRequestValidator.java
@@ -1,6 +1,9 @@
 package org.hypertrace.notification.config.service;
 
+import static org.hypertrace.config.validation.GrpcValidatorUtils.validateRequestContextOrThrow;
+
 import com.google.common.base.Preconditions;
+import org.hypertrace.config.validation.GrpcValidatorUtils;
 import org.hypertrace.core.grpcutils.context.RequestContext;
 import org.hypertrace.notification.config.service.v1.CreateNotificationChannelRequest;
 import org.hypertrace.notification.config.service.v1.CreateNotificationRuleRequest;
@@ -15,41 +18,41 @@ public class NotificationConfigServiceRequestValidator {
 
   public void validateCreateNotificationRuleRequest(
       RequestContext requestContext, CreateNotificationRuleRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         request.hasNewNotificationRule(), "Notification rule should be present");
     Preconditions.checkArgument(
-        !request.getNewNotificationRule().getRuleName().isBlank(), "Rule name cannot be empty");
+        !request.getNewNotificationRule().getNotificationRuleData().getRuleName().isBlank(), "Rule name cannot be empty");
     Preconditions.checkArgument(
-        !request.getNewNotificationRule().getChannelId().isBlank(), "ChannelId cannot be empty");
+        !request.getNewNotificationRule().getNotificationRuleData().getChannelId().isBlank(), "ChannelId cannot be empty");
   }
 
   public void validateUpdateNotificationRuleRequest(
       RequestContext requestContext, UpdateNotificationRuleRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         request.hasNotificationRule(), "Notification rule should be present");
     Preconditions.checkArgument(
-        !request.getNotificationRule().getRuleName().isBlank(), "Rule name cannot be empty");
+        !request.getNotificationRule().getNotificationRuleData().getRuleName().isBlank(), "Rule name cannot be empty");
     Preconditions.checkArgument(
-        !request.getNotificationRule().getChannelId().isBlank(), "ChannelId cannot be empty");
+        !request.getNotificationRule().getNotificationRuleData().getChannelId().isBlank(), "ChannelId cannot be empty");
   }
 
   public void validateGetAllNotificationRulesRequest(
       RequestContext requestContext, GetAllNotificationRulesRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
   }
 
   public void validateDeleteNotificationRuleRequest(
       RequestContext requestContext, DeleteNotificationRuleRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         !request.getNotificationRuleId().isBlank(), "RuleId cannot be empty");
   }
 
   public void validateCreateNotificationChannelRequest(
       RequestContext requestContext, CreateNotificationChannelRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         request.hasNewNotificationChannel(), "Notification channel should be present");
     Preconditions.checkArgument(
@@ -62,7 +65,7 @@ public class NotificationConfigServiceRequestValidator {
 
   public void validateUpdateNotificationChannelRequest(
       RequestContext requestContext, UpdateNotificationChannelRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         request.hasNotificationChannel(), "Notification channel should be present");
     Preconditions.checkArgument(
@@ -75,18 +78,13 @@ public class NotificationConfigServiceRequestValidator {
 
   public void validateGetAllNotificationChannelsRequest(
       RequestContext requestContext, GetAllNotificationChannelsRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
   }
 
   public void validateDeleteNotificationChannelRequest(
       RequestContext requestContext, DeleteNotificationChannelRequest request) {
-    validateTenantID(requestContext);
+    validateRequestContextOrThrow(requestContext);
     Preconditions.checkArgument(
         !request.getNotificationChannelId().isBlank(), "ChannelId cannot be empty");
-  }
-
-  private void validateTenantID(RequestContext requestContext) {
-    Preconditions.checkArgument(
-        requestContext.getTenantId().isPresent(), "Tenant ID is not present in request context");
   }
 }
