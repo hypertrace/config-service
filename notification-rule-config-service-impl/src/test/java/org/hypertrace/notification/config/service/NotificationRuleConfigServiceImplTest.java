34b5efe116cf0a6db556b87a68ccac2dadefa382
rish691 <rishabh@traceable.ai>
2021-09-24 22:49:37 +0530

    spotless

--- notification-rule-config-service-impl/src/test/java/org/hypertrace/notification/config/service/NotificationRuleConfigServiceImplTest.java
+++ notification-rule-config-service-impl/src/test/java/org/hypertrace/notification/config/service/NotificationRuleConfigServiceImplTest.java
@@ -1,6 +1,5 @@
 package org.hypertrace.notification.config.service;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
