211845f7a5cad5d87633ac84596b25c2979f0462
SrikarMannepalli <srikar.mannepalli@traceable.ai>
2021-11-02 11:36:22 +0530

    remove unused object

--- object-store/src/test/java/org/hypertrace/config/objectstore/IdentifiedObjectStoreTest.java
+++ object-store/src/test/java/org/hypertrace/config/objectstore/IdentifiedObjectStoreTest.java
@@ -249,13 +249,6 @@ class IdentifiedObjectStoreTest {
     int rank;
   }
 
-  @lombok.Value
-  @Builder
-  private static class MyObject {
-    String id;
-    int rank;
-  }
-
   private static class TestObjectStore extends IdentifiedObjectStore<TestObject> {
     private TestObjectStore(
         ConfigServiceBlockingStub stub, ConfigChangeEventGenerator configChangeEventGenerator) {
