ddb3c12dc0cf70bf3fc828a5ee807c8c9cb12631
SrikarMannepalli <srikar.mannepalli@traceable.ai>
2022-03-10 15:35:13 +0530

    update indentation

--- config-utils/build.gradle.kts
+++ config-utils/build.gradle.kts
@@ -1,22 +1,22 @@
 plugins {
-    `java-library`
-    jacoco
-    id("org.hypertrace.jacoco-report-plugin")
+  `java-library`
+  jacoco
+  id("org.hypertrace.jacoco-report-plugin")
 }
 
 dependencies {
-    api(projects.spanProcessingConfigServiceApi)
-    implementation(libs.slf4j.api)
+  api(projects.spanProcessingConfigServiceApi)
+  implementation(libs.slf4j.api)
 
-    annotationProcessor(libs.lombok)
-    compileOnly(libs.lombok)
+  annotationProcessor(libs.lombok)
+  compileOnly(libs.lombok)
 
-    testImplementation(libs.junit.jupiter)
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.mockito.junit)
-    testImplementation(testFixtures(projects.configServiceApi))
+  testImplementation(libs.junit.jupiter)
+  testImplementation(libs.mockito.core)
+  testImplementation(libs.mockito.junit)
+  testImplementation(testFixtures(projects.configServiceApi))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
