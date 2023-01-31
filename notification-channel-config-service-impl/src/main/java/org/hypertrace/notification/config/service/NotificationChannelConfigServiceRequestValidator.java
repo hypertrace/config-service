ed4d0dbd97a835450ed8ee844502065890c1252f
Apoorv Jain <apoorv.jain@traceable.ai>
2023-01-31 16:34:33 +0530

    aws s3 bucket channel (#156)
    * aws s3 bucket channel
    
    * repeated
    
    * pr comments
    
    * nit
    
    * validationg
    
    * nit
    
    ---------
    
    Co-authored-by: Apoorv Jain <apoorvjain@Apoorvs-MacBook-Pro.local>

--- notification-channel-config-service-impl/src/main/java/org/hypertrace/notification/config/service/NotificationChannelConfigServiceRequestValidator.java
+++ notification-channel-config-service-impl/src/main/java/org/hypertrace/notification/config/service/NotificationChannelConfigServiceRequestValidator.java
@@ -5,12 +5,17 @@ import static org.hypertrace.config.validation.GrpcValidatorUtils.validateReques
 
 import io.grpc.Status;
 import org.hypertrace.core.grpcutils.context.RequestContext;
+import org.hypertrace.notification.config.service.v1.AwsS3BucketChannelConfig;
+import org.hypertrace.notification.config.service.v1.AwsS3BucketChannelConfig.WebIdentityAuthenticationCredential;
 import org.hypertrace.notification.config.service.v1.CreateNotificationChannelRequest;
 import org.hypertrace.notification.config.service.v1.DeleteNotificationChannelRequest;
+import org.hypertrace.notification.config.service.v1.EmailChannelConfig;
 import org.hypertrace.notification.config.service.v1.GetAllNotificationChannelsRequest;
 import org.hypertrace.notification.config.service.v1.GetNotificationChannelRequest;
 import org.hypertrace.notification.config.service.v1.NotificationChannelMutableData;
 import org.hypertrace.notification.config.service.v1.UpdateNotificationChannelRequest;
+import org.hypertrace.notification.config.service.v1.WebhookChannelConfig;
+import org.hypertrace.notification.config.service.v1.WebhookHeader;
 
 public class NotificationChannelConfigServiceRequestValidator {
 
@@ -30,11 +35,14 @@ public class NotificationChannelConfigServiceRequestValidator {
   private void validateNotificationChannelMutableData(NotificationChannelMutableData data) {
     validateNonDefaultPresenceOrThrow(
         data, NotificationChannelMutableData.CHANNEL_NAME_FIELD_NUMBER);
-    if (data.getEmailChannelConfigCount() == 0 && data.getWebhookChannelConfigCount() == 0) {
-      throw Status.INVALID_ARGUMENT
-          .withDescription("Either email or webhook config should be present")
-          .asRuntimeException();
+    if (data.getEmailChannelConfigCount() == 0
+        && data.getWebhookChannelConfigCount() == 0
+        && data.getS3BucketChannelConfigCount() == 0) {
+      throw Status.INVALID_ARGUMENT.withDescription("No config present").asRuntimeException();
     }
+    data.getEmailChannelConfigList().forEach(this::validateEmailChannelConfig);
+    data.getWebhookChannelConfigList().forEach(this::validateWebhookChannelConfig);
+    data.getS3BucketChannelConfigList().forEach(this::validateS3BucketConfig);
   }
 
   public void validateGetAllNotificationChannelsRequest(
@@ -54,5 +62,38 @@ public class NotificationChannelConfigServiceRequestValidator {
     validateRequestContextOrThrow(requestContext);
     validateNonDefaultPresenceOrThrow(
         request, GetNotificationChannelRequest.NOTIFICATION_CHANNEL_ID_FIELD_NUMBER);
+  }
+
+  private void validateEmailChannelConfig(EmailChannelConfig emailChannelConfig) {
+    validateNonDefaultPresenceOrThrow(emailChannelConfig, EmailChannelConfig.ADDRESS_FIELD_NUMBER);
+  }
+
+  private void validateWebhookChannelConfig(WebhookChannelConfig webhookChannelConfig) {
+    validateNonDefaultPresenceOrThrow(webhookChannelConfig, WebhookChannelConfig.URL_FIELD_NUMBER);
+    validateNonDefaultPresenceOrThrow(
+        webhookChannelConfig, WebhookChannelConfig.FORMAT_FIELD_NUMBER);
+    webhookChannelConfig.getHeadersList().forEach(this::validateWebhookHeader);
+  }
+
+  private void validateS3BucketConfig(AwsS3BucketChannelConfig awsS3BucketChannelConfig) {
+    validateNonDefaultPresenceOrThrow(
+        awsS3BucketChannelConfig, AwsS3BucketChannelConfig.BUCKET_NAME_FIELD_NUMBER);
+    validateNonDefaultPresenceOrThrow(
+        awsS3BucketChannelConfig, AwsS3BucketChannelConfig.REGION_FIELD_NUMBER);
+    switch (awsS3BucketChannelConfig.getAuthenticationCredentialCase()) {
+      case WEB_IDENTITY_AUTH_CREDENTIAL:
+        validateNonDefaultPresenceOrThrow(
+            awsS3BucketChannelConfig.getWebIdentityAuthCredential(),
+            WebIdentityAuthenticationCredential.ROLE_ARN_FIELD_NUMBER);
+        break;
+      default:
+        throw Status.INVALID_ARGUMENT
+            .withDescription("No credentials present in AWS S3 bucket config")
+            .asRuntimeException();
+    }
+  }
+
+  private void validateWebhookHeader(WebhookHeader webhookHeader) {
+    validateNonDefaultPresenceOrThrow(webhookHeader, WebhookHeader.NAME_FIELD_NUMBER);
   }
 }
