b8a62b04a2db29605b5a95b7f6e6c0bac6c6f3e0
arun-dhingra <arun.dhingra@traceable.ai>
2023-06-03 13:18:55 +0530

    ENG-25560: fixes linting.

--- partitioner-config-service-impl/src/main/java/org/hypertrace/partitioner/config/service/store/PartitionerProfilesDocumentStore.java
+++ partitioner-config-service-impl/src/main/java/org/hypertrace/partitioner/config/service/store/PartitionerProfilesDocumentStore.java
@@ -5,7 +5,6 @@ import com.typesafe.config.Config;
 import io.grpc.Status;
 import java.util.*;
 import java.util.stream.Collectors;
-
 import lombok.SneakyThrows;
 import org.hypertrace.core.documentstore.*;
 import org.hypertrace.core.documentstore.Collection;
@@ -75,17 +74,16 @@ public class PartitionerProfilesDocumentStore implements PartitionerProfilesStor
   @SneakyThrows
   private void setUpDefaultProfile(Config partitionerConfig) {
     ArrayList<PartitionerProfile> partitionerProfiles = new ArrayList<>();
-    for (Config profile : partitionerConfig
-            .getConfigList(DEFAULT_PROFILES)) {
+    for (Config profile : partitionerConfig.getConfigList(DEFAULT_PROFILES)) {
       Optional<PartitionerProfile> fetchedProfile =
-              getPartitionerProfile(profile.getString(PROFILE_NAME));
+          getPartitionerProfile(profile.getString(PROFILE_NAME));
       if (fetchedProfile.isEmpty()) {
         PartitionerProfile newProfile =
-                PartitionerProfile.newBuilder()
-                        .setName(profile.getString(PROFILE_NAME))
-                        .setDefaultGroupWeight(DEFAULT_PROFILE_WEIGHT)
-                        .setPartitionKey(profile.getString(PROFILE_PARTITION_KEY))
-                        .build();
+            PartitionerProfile.newBuilder()
+                .setName(profile.getString(PROFILE_NAME))
+                .setDefaultGroupWeight(DEFAULT_PROFILE_WEIGHT)
+                .setPartitionKey(profile.getString(PROFILE_PARTITION_KEY))
+                .build();
         partitionerProfiles.add(newProfile);
         putPartitionerProfiles(partitionerProfiles);
       }
