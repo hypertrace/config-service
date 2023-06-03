a535a0f301d53870a909f356560e27debd4c59b0
arun-dhingra <arun.dhingra@traceable.ai>
2023-06-03 12:47:22 +0530

    ENG-25560: Updates error handling for default profile setup.

--- partitioner-config-service-impl/src/main/java/org/hypertrace/partitioner/config/service/store/PartitionerProfilesDocumentStore.java
+++ partitioner-config-service-impl/src/main/java/org/hypertrace/partitioner/config/service/store/PartitionerProfilesDocumentStore.java
@@ -5,6 +5,8 @@ import com.typesafe.config.Config;
 import io.grpc.Status;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import lombok.SneakyThrows;
 import org.hypertrace.core.documentstore.*;
 import org.hypertrace.core.documentstore.Collection;
 import org.hypertrace.partitioner.config.service.v1.PartitionerProfile;
@@ -70,28 +72,23 @@ public class PartitionerProfilesDocumentStore implements PartitionerProfilesStor
         profiles.stream().map(PartitionerProfileKey::new).collect(Collectors.toSet()));
   }
 
+  @SneakyThrows
   private void setUpDefaultProfile(Config partitionerConfig) {
     ArrayList<PartitionerProfile> partitionerProfiles = new ArrayList<>();
-    partitionerConfig
-        .getConfigList(DEFAULT_PROFILES)
-        .forEach(
-            profile -> {
-              try {
-                Optional<PartitionerProfile> fetchedProfile =
-                    getPartitionerProfile(profile.getString(PROFILE_NAME));
-                if (fetchedProfile.isEmpty()) {
-                  PartitionerProfile newProfile =
-                      PartitionerProfile.newBuilder()
-                          .setName(profile.getString(PROFILE_NAME))
-                          .setDefaultGroupWeight(DEFAULT_PROFILE_WEIGHT)
-                          .setPartitionKey(profile.getString(PROFILE_PARTITION_KEY))
-                          .build();
-                  partitionerProfiles.add(newProfile);
-                  putPartitionerProfiles(partitionerProfiles);
-                }
-              } catch (Exception e) {
-                throw new RuntimeException(e);
-              }
-            });
+    for (Config profile : partitionerConfig
+            .getConfigList(DEFAULT_PROFILES)) {
+      Optional<PartitionerProfile> fetchedProfile =
+              getPartitionerProfile(profile.getString(PROFILE_NAME));
+      if (fetchedProfile.isEmpty()) {
+        PartitionerProfile newProfile =
+                PartitionerProfile.newBuilder()
+                        .setName(profile.getString(PROFILE_NAME))
+                        .setDefaultGroupWeight(DEFAULT_PROFILE_WEIGHT)
+                        .setPartitionKey(profile.getString(PROFILE_PARTITION_KEY))
+                        .build();
+        partitionerProfiles.add(newProfile);
+        putPartitionerProfiles(partitionerProfiles);
+      }
+    }
   }
 }
